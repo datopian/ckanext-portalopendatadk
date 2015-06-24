170fc6f5edfd75180c7b954cb86d3f12cf7c5518
Henrik Aagaard Jørgensen <haj@cphsolutionslab.dk>
2015-06-24 09:26:31 +0200

    Updated footer link for Aalborg

--- ckanext/portalopendatadk/fanstatic/ga.js
+++ ckanext/portalopendatadk/fanstatic/ga.js
@@ -4,7 +4,7 @@
   m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
   })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-  ga('create', 'UA-57924573-3', 'auto');
+  ga('create', 'UA-63709238-1', 'auto');
   ga('send', 'pageview');
 
 </script>
