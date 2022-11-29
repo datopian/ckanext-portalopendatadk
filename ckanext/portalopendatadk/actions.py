385208b19abf88a92e7971c3a2afc4dd3fdf631a
geoffrey1330 <israelgeoffrey13@gmail.com>
2022-11-29 18:38:27 +0100

    Added log message

--- ckanext/portalopendatadk/actions.py
+++ ckanext/portalopendatadk/actions.py
@@ -280,6 +280,8 @@ def package_update(context, data_dict):
               dataset id)
     :rtype: dictionary
     '''
+
+    log.error(data_dict)
     model = context['model']
     user = context['user']
     name_or_id = data_dict.get('id') or data_dict.get('name')
@@ -376,4 +378,5 @@ def package_update(context, data_dict):
     output = data_dict['id'] if return_id_only \
         else _get_action('package_show')(context, {'id': data_dict['id']})
 
+    log.error(output)
     return output
