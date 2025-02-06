6dde04f0f60d71daa82c04c85b2a290997b139a6
Michael Polidori <contact@michaelpolidori.com>
2025-02-06 13:25:46 -0500

    Initial changes for plugin to load successfully, without functionality

--- ckanext/portalopendatadk/plugin.py
+++ ckanext/portalopendatadk/plugin.py
@@ -7,8 +7,9 @@ from ckan.logic.action.create import user_create as core_user_create
 from ckan.logic.action.update import user_update as core_user_update
 from ckan.lib import mailer
 from ckan.lib.plugins import DefaultTranslation
-from pylons import config
+#from pylons import config
 from ckan import authz
+from ckan.common import config
 import logging
 
 from ckanext.portalopendatadk import actions as oddk_actions
@@ -53,92 +54,92 @@ class PortalOpenDataDKPlugin(
 ):
     """portal.opendata.dk theme plugin."""
 
-    plugins.implements(plugins.IConfigurer)
-    plugins.implements(plugins.ITemplateHelpers)
-    plugins.implements(plugins.IActions)
-    plugins.implements(plugins.ITranslation)
-    plugins.implements(plugins.IDatasetForm, inherit=True)
-    plugins.implements(plugins.IFacets)
-    plugins.implements(plugins.IRoutes, inherit=True)
-    plugins.implements(plugins.IAuthFunctions, inherit=True)
-    plugins.implements(plugins.IValidators, inherit=True)
-    plugins.implements(plugins.IPackageController, inherit=True)
+    #plugins.implements(plugins.IConfigurer)
+    #plugins.implements(plugins.ITemplateHelpers)
+    #plugins.implements(plugins.IActions)
+    #plugins.implements(plugins.ITranslation)
+    #plugins.implements(plugins.IDatasetForm, inherit=True)
+    #plugins.implements(plugins.IFacets)
+    #plugins.implements(plugins.IRoutes, inherit=True)
+    #plugins.implements(plugins.IAuthFunctions, inherit=True)
+    #plugins.implements(plugins.IValidators, inherit=True)
+    #plugins.implements(plugins.IPackageController, inherit=True)
 
-    def before_map(self, map):
-        # Pass requests to ODDKUserController to verify admin status
-        map.connect(
-            '/user',
-            controller='ckanext.portalopendatadk.controller:ODDKUserController',
-            action='index',
-        )
-        map.connect(
-            '/user/register',
-            controller='ckanext.portalopendatadk.controller:ODDKUserController',
-            action='register',
-        )
-        map.connect(
-            '/user/edit',
-            controller='ckanext.portalopendatadk.controller:ODDKUserController',
-            action='edit',
-        )
-        map.connect(
-            '/user/edit/{id:.*}',
-            controller='ckanext.portalopendatadk.controller:ODDKUserController',
-            action='edit',
-        )
-        map.connect(
-            '/user/reset',
-            controller='ckanext.portalopendatadk.controller:ODDKUserController',
-            action='request_reset',
-        )
+    #def before_map(self, map):
+    #    # Pass requests to ODDKUserController to verify admin status
+    #    map.connect(
+    #        '/user',
+    #        controller='ckanext.portalopendatadk.controller:ODDKUserController',
+    #        action='index',
+    #    )
+    #    map.connect(
+    #        '/user/register',
+    #        controller='ckanext.portalopendatadk.controller:ODDKUserController',
+    #        action='register',
+    #    )
+    #    map.connect(
+    #        '/user/edit',
+    #        controller='ckanext.portalopendatadk.controller:ODDKUserController',
+    #        action='edit',
+    #    )
+    #    map.connect(
+    #        '/user/edit/{id:.*}',
+    #        controller='ckanext.portalopendatadk.controller:ODDKUserController',
+    #        action='edit',
+    #    )
+    #    map.connect(
+    #        '/user/reset',
+    #        controller='ckanext.portalopendatadk.controller:ODDKUserController',
+    #        action='request_reset',
+    #    )
 
-        dcat_controller = 'ckanext.portalopendatadk.controller:DCATController'
+    #    dcat_controller = 'ckanext.portalopendatadk.controller:DCATController'
 
-        if toolkit.asbool(config.get(ENABLE_RDF_ENDPOINTS_CONFIG, True)):
-            map.connect(
-                'dcat_catalog',
-                config.get('ckanext.dcat.catalog_endpoint', DEFAULT_CATALOG_ENDPOINT),
-                controller=dcat_controller,
-                action='read_catalog',
-                requirements={'_format': 'xml|rdf|n3|ttl|jsonld'},
-            )
+    #    if toolkit.asbool(config.get(ENABLE_RDF_ENDPOINTS_CONFIG, True)):
+    #        map.connect(
+    #            'dcat_catalog',
+    #            config.get('ckanext.dcat.catalog_endpoint', DEFAULT_CATALOG_ENDPOINT),
+    #            controller=dcat_controller,
+    #            action='read_catalog',
+    #            requirements={'_format': 'xml|rdf|n3|ttl|jsonld'},
+    #        )
 
-            map.connect(
-                'dcat_dataset',
-                '/dataset/{_id}.{_format}',
-                controller=dcat_controller,
-                action='read_dataset',
-                requirements={'_format': 'xml|rdf|n3|ttl|jsonld'},
-            )
+    #        map.connect(
+    #            'dcat_dataset',
+    #            '/dataset/{_id}.{_format}',
+    #            controller=dcat_controller,
+    #            action='read_dataset',
+    #            requirements={'_format': 'xml|rdf|n3|ttl|jsonld'},
+    #        )
 
-        if toolkit.asbool(config.get(ENABLE_CONTENT_NEGOTIATION_CONFIG)):
-            map.connect('home', '/', controller=dcat_controller, action='read_catalog')
+    #    if toolkit.asbool(config.get(ENABLE_CONTENT_NEGOTIATION_CONFIG)):
+    #        map.connect('home', '/', controller=dcat_controller, action='read_catalog')
 
-            map.connect(
-                'add dataset', '/dataset/new', controller='package', action='new'
-            )
-            map.connect(
-                'dataset_read',
-                '/dataset/{_id}',
-                controller=dcat_controller,
-                action='read_dataset',
-                ckan_icon='sitemap',
-            )
+    #        map.connect(
+    #            'add dataset', '/dataset/new', controller='package', action='new'
+    #        )
+    #        map.connect(
+    #            'dataset_read',
+    #            '/dataset/{_id}',
+    #            controller=dcat_controller,
+    #            action='read_dataset',
+    #            ckan_icon='sitemap',
+    #        )
 
-        documentation_controller = (
-            'ckanext.portalopendatadk.controller:ODDKDocumentationController'
-        )
+    #    documentation_controller = (
+    #        'ckanext.portalopendatadk.controller:ODDKDocumentationController'
+    #    )
 
-        map.connect(
-            '/dataset/{id}/documentation/{resource_id}',
-            controller=documentation_controller,
-            action='documentation_download',
-        )
+    #    map.connect(
+    #        '/dataset/{id}/documentation/{resource_id}',
+    #        controller=documentation_controller,
+    #        action='documentation_download',
+    #    )
 
-        return map
+    #    return map
 
-    def after_map(self, map):
-        return map
+    #def after_map(self, map):
+    #    return map
 
     def update_config(self, config):
         toolkit.add_template_directory(config, 'templates')
@@ -159,9 +160,9 @@ class PortalOpenDataDKPlugin(
 
     def get_actions(self):
         return {
-            'user_create': custom_user_create,
-            'user_update': custom_user_update,
-            'get_user_email': get_user_email,
+            #'user_create': custom_user_create,
+            #'user_update': custom_user_update,
+            #'get_user_email': get_user_email,
             'package_update': oddk_actions.package_update,
             'package_create': oddk_actions.package_create,
             'package_search': oddk_actions.package_search,
@@ -231,116 +232,116 @@ class PortalOpenDataDKPlugin(
 # Custom actions
 
 
-def custom_user_create(context, data_dict):
-    context['schema'] = custom_create_user_schema(
-        form_schema='password1' in context.get('schema', {})
-    )
+#def custom_user_create(context, data_dict):
+#    context['schema'] = custom_create_user_schema(
+#        form_schema='password1' in context.get('schema', {})
+#    )
+#
+#    return core_user_create(context, data_dict)
 
-    return core_user_create(context, data_dict)
 
-
-def custom_user_update(context, data_dict):
-    context['schema'] = custom_update_user_schema(
-        form_schema='password1' in context.get('schema', {})
-    )
-
-    return core_user_update(context, data_dict)
+#def custom_user_update(context, data_dict):
+#    context['schema'] = custom_update_user_schema(
+#        form_schema='password1' in context.get('schema', {})
+#    )
+#
+#    return core_user_update(context, data_dict)
 
 
 # Custom schemas
 
 
-def custom_create_user_schema(form_schema=False):
-    schema = default_user_schema()
-
-    schema['password'] = [
-        custom_user_password_validator,
-        toolkit.get_validator('user_password_not_empty'),
-        toolkit.get_validator('ignore_missing'),
-        unicode,
-    ]
-
-    if form_schema:
-        schema['password1'] = [
-            toolkit.get_validator('user_both_passwords_entered'),
-            custom_user_password_validator,
-            toolkit.get_validator('user_passwords_match'),
-            unicode,
-        ]
-        schema['password2'] = [unicode]
-
-    return schema
-
-
-def custom_update_user_schema(form_schema=False):
-    schema = default_update_user_schema()
-
-    schema['password'] = [
-        custom_user_password_validator,
-        toolkit.get_validator('user_password_not_empty'),
-        toolkit.get_validator('ignore_missing'),
-        unicode,
-    ]
-
-    if form_schema:
-        schema['password'] = [toolkit.get_validator('ignore_missing')]
-        schema['password1'] = [
-            toolkit.get_validator('ignore_missing'),
-            custom_user_password_validator,
-            toolkit.get_validator('user_passwords_match'),
-            unicode,
-        ]
-        schema['password2'] = [toolkit.get_validator('ignore_missing'), unicode]
-
-    return schema
-
-
-# Custom validators
-WRONG_PASSWORD_MESSAGE = (
-    'Your password must be 8 characters or longer, '
-    + 'contain at least one capital letter, one small letter, '
-    + 'one number(0-9) and a '
-    + 'special_character(!"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~)'
-)
-
-
-def custom_user_password_validator(key, data, errors, context):
-    value = data[key]
-    special_chars = string.punctuation
-
-    if isinstance(value, Missing):
-        pass
-    elif not isinstance(value, basestring):
-        errors[('password',)].append(_('Passwords must be strings'))
-    elif value == '':
-        pass
-    elif (
-        len(value) < 8
-        or not any(x.isdigit() for x in value)
-        or not any(x.isupper() for x in value)
-        or not any(x.islower() for x in value)
-        or not any(x in special_chars for x in value)
-    ):
-        errors[('password',)].append(_(WRONG_PASSWORD_MESSAGE))
-
-
-@toolkit.side_effect_free
-def get_user_email(context, data_dict):
-    """
-    Returns the user names and emails of all the users
-    """
-    if not authz.is_sysadmin(toolkit.c.user):
-        toolkit.abort(403, _('You are not authorized to access this list'))
-
-    user_list = toolkit.get_action('user_list')(context, data_dict)
-    user_name_email = []
-
-    for user in user_list:
-        email = user['email']
-        user_name = user['display_name']
-        user_name_email.append({'user_name': user_name, 'email_address': email})
-
-    return user_name_email
+#def custom_create_user_schema(form_schema=False):
+#    schema = default_user_schema()
+#
+#    schema['password'] = [
+#        custom_user_password_validator,
+#        toolkit.get_validator('user_password_not_empty'),
+#        toolkit.get_validator('ignore_missing'),
+#        str,
+#    ]
+#
+#    if form_schema:
+#        schema['password1'] = [
+#            toolkit.get_validator('user_both_passwords_entered'),
+#            custom_user_password_validator,
+#            toolkit.get_validator('user_passwords_match'),
+#            str,
+#        ]
+#        schema['password2'] = [str]
+#
+#    return schema
+#
+#
+#def custom_update_user_schema(form_schema=False):
+#    schema = default_update_user_schema()
+#
+#    schema['password'] = [
+#        custom_user_password_validator,
+#        toolkit.get_validator('user_password_not_empty'),
+#        toolkit.get_validator('ignore_missing'),
+#        str,
+#    ]
+#
+#    if form_schema:
+#        schema['password'] = [toolkit.get_validator('ignore_missing')]
+#        schema['password1'] = [
+#            toolkit.get_validator('ignore_missing'),
+#            custom_user_password_validator,
+#            toolkit.get_validator('user_passwords_match'),
+#            str,
+#        ]
+#        schema['password2'] = [toolkit.get_validator('ignore_missing'), str]
+#
+#    return schema
+#
+#
+## Custom validators
+#WRONG_PASSWORD_MESSAGE = (
+#    'Your password must be 8 characters or longer, '
+#    + 'contain at least one capital letter, one small letter, '
+#    + 'one number(0-9) and a '
+#    + 'special_character(!"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~)'
+#)
+#
+#
+#def custom_user_password_validator(key, data, errors, context):
+#    value = data[key]
+#    special_chars = string.punctuation
+#
+#    if isinstance(value, Missing):
+#        pass
+#    elif not isinstance(value, str):
+#        errors[('password',)].append(_('Passwords must be strings'))
+#    elif value == '':
+#        pass
+#    elif (
+#        len(value) < 8
+#        or not any(x.isdigit() for x in value)
+#        or not any(x.isupper() for x in value)
+#        or not any(x.islower() for x in value)
+#        or not any(x in special_chars for x in value)
+#    ):
+#        errors[('password',)].append(_(WRONG_PASSWORD_MESSAGE))
+#
+#
+#@toolkit.side_effect_free
+#def get_user_email(context, data_dict):
+#    """
+#    Returns the user names and emails of all the users
+#    """
+#    if not authz.is_sysadmin(toolkit.c.user):
+#        toolkit.abort(403, _('You are not authorized to access this list'))
+#
+#    user_list = toolkit.get_action('user_list')(context, data_dict)
+#    user_name_email = []
+#
+#    for user in user_list:
+#        email = user['email']
+#        user_name = user['display_name']
+#        user_name_email.append({'user_name': user_name, 'email_address': email})
+#
+#    return user_name_email
 
 
 def get_update_frequencies():
